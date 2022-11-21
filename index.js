3883bd54734136d30261d89ce8473c54c42d5dcd
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-11-20 21:57:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var x = prompt('Digite seu ano de nascimento')
+let y =  2022
+var w = prompt('Digite seu nome')
+var idade = y-x
+ let mensagem = `Olá seu nome é  ${w} e você tem ${idade} anos`
+ alert(mensagem)
+
+
+
